1f8cc0eaed70cc6508f3568ba5fbd68a3ad24294
mazohere <akira.harris@education.nsw.gov.au>
2021-06-15 12:38:35 +1000

    payger YOU

--- Assets/TypingScript.cs
+++ Assets/TypingScript.cs
@@ -7,13 +7,33 @@ public class TypingScript : MonoBehaviour
 {
     public Text questionText;
     public InputField answerField;
+    public Button answerVerification;
+    public int correctAnswer;
 
-    
+    public void Question()
+    {
+        int firstnumber = Random.Range(1, 13);
+        int secondnumber = Random.Range(1, 13);
+        correctAnswer = (firstnumber * secondnumber);
+        questionText.text = (firstnumber + " * " + secondnumber).ToString();
+
+    }
+
+    public void OnButtonClick()
+    {
+        if (answerField.text == (correctAnswer).ToString())
+        {
+            print("correct");
+        } else
+        {
+            print("incorrect");
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Question();
     }
 
     // Update is called once per frame
