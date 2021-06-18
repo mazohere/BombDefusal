2be5127fd8c715f9dfae4d486bb813877ff6cfd2
mazohere <akira.harris@education.nsw.gov.au>
2021-06-18 15:11:39 +1000

    better than before

--- Assets/ButtonSelectScript.cs
+++ Assets/ButtonSelectScript.cs
@@ -5,80 +5,97 @@ using UnityEngine.UI;
 
 public class ButtonSelectScript : MonoBehaviour
 {
-    public Text Question1;
-    public Text Question2;
-    public Text Question3;
-    public Text Answer1;
-    public Text Answer2;
-    public Text Answer3;
-    public Button question1Button;
-    public Button question2Button;
-    public Button question3Button;
-    public Button answer1Button;
-    public Button answer2Button;
-    public Button answer3Button;
+    //public Text Question1;
+    //public Text Question2;
+    //public Text Question3;
+    //public Text Answer1;
+    //public Text Answer2;
+    //public Text Answer3;
+    //public Button question1Button;
+    //public Button question2Button;
+    //public Button question3Button;
+    //public Button answer1Button;
+    //public Button answer2Button;
+    //public Button answer3Button;
 
-    public void QuestionAssignment()
+    public List<Button> questions;
+    public List<Button> answers;
+    private static System.Random rng = new System.Random();
+
+    //public void QuestionAssignment()
+    //{
+    //    //Q = Question; FN = first number; SN = second number; QT = question text; QA = question answer; Q_S = question _ select
+
+    //    int Q1FN = Random.Range(1, 13);
+    //    int Q1SN = Random.Range(1, 13);
+    //    int Q2FN = Random.Range(1, 13);
+    //    int Q2SN = Random.Range(1, 13);
+    //    int Q3FN = Random.Range(1, 13);
+    //    int Q3SN = Random.Range(1, 13);
+    //    string QT1 = (Q1FN + " * " + Q1SN);
+    //    string QT2 = Q2FN + " * " + Q2SN;
+    //    string QT3 = Q3FN + " * " + Q3SN;
+    //    int QA1 = Q1FN * Q1SN;
+    //    int QA2 = Q2FN * Q2SN;
+    //    int QA3 = Q3FN * Q3SN;
+    //    print(" this is for the button one: " + QT1);
+    //    Question1.text = QT1;
+    //    Question2.text = QT2;
+    //    Question3.text = QT3;
+    //    Answer1.text = QA1.ToString();
+    //    Answer2.text = QA2.ToString();
+    //    Answer3.text = QA3.ToString();
+
+    //    int Q1S = Random.Range(0, 3);
+    //    int Q2S = Random.Range(0, 3);
+    //    int Q3S = Random.Range(0, 3);
+
+    //    print("Q1S: " + Q1S);
+    //    print("Q2S: " + Q2S);
+    //    print("Q3S: " + Q3S);
+
+
+    //}
+
+    public void GenerateQuestions()
     {
-        //Q = Question; FN = first number; SN = second number; QT = question text; QA = question answer; Q_S = question _ select
+        List<int> actualAnswers = new List<int>();
 
-        int Q1FN = Random.Range(1, 13);
-        int Q1SN = Random.Range(1, 13);
-        int Q2FN = Random.Range(1, 13);
-        int Q2SN = Random.Range(1, 13);
-        int Q3FN = Random.Range(1, 13);
-        int Q3SN = Random.Range(1, 13);
-        string QT1 = (Q1FN + " * " + Q1SN);
-        string QT2 = Q2FN + " * " + Q2SN;
-        string QT3 = Q3FN + " * " + Q3SN;
-        int QA1 = Q1FN * Q1SN;
-        int QA2 = Q2FN * Q2SN;
-        int QA3 = Q3FN * Q3SN;
-        print(" this is for the button one: " + QT1);
-        Question1.text = QT1;
-        Question2.text = QT2;
-        Question3.text = QT3;
-        Answer1.text = QA1.ToString();
-        Answer2.text = QA2.ToString();
-        Answer3.text = QA3.ToString();
-
-        int Q1S = Random.Range(0, 3);
-        int Q2S = Random.Range(0, 3);
-        if (Q2S == Q1S && Q1S == 2)
+        for (int i = 0; i < questions.Count; i++)
         {
-            Q2S = Q2S - 1;
-        }
-        if (Q2S == Q1S && Q1S == 0)
-        {
-            Q2S = Q2S + 1;
+            int num1 = Random.Range(1, 13);
+            int num2 = Random.Range(1, 13);
+            questions[i].GetComponentInChildren<Text>().text = num1 + " x " + num2;
+            questions[i].name = (num1 * num2).ToString();
+            actualAnswers.Add(num1 * num2);
         }
 
-        int Q3S = 0;
-        if (Q1S == 0 || Q2S == 0)
-        {
-            Q3S = Q3S + 1;
-        }
-        if (Q1S == 1 || Q2S == 1)
-        {
-            Q3S = Q3S + 1;
-        }
-        if (Q1S == 2 || Q2S == 2)
-        {
-            Q3S = Q3S + 1;
-        }
+        Shuffle(actualAnswers);
 
-        print("Q1S: " + Q1S);
-        print("Q2S: " + Q2S);
-        print("Q3S: " + Q3S);
-
-
+        for (int i = 0; i < answers.Count; i++)
+        {
+            answers[i].GetComponentInChildren<Text>().text = actualAnswers[i].ToString();
+        }
     }
 
+    public static void Shuffle(List<int> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            int value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        QuestionAssignment();
+        //QuestionAssignment();
+        GenerateQuestions();
         print("gaming");
     }
 
